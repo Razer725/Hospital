17eec03b325f6b9ed9417702f887050c80e06290
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-03 14:13:40 +0300

    Add zero check to request_patient_id

--- user_interaction.py
+++ user_interaction.py
@@ -7,7 +7,7 @@ class UserInteraction:
 
     def request_patient_id(self):
         patient_id = input("Введите ID пациента: ")
-        if not patient_id.isdigit():
+        if not patient_id.isdigit() or int(patient_id) == 0:
             raise PatientIdTypeError
         return patient_id
 
