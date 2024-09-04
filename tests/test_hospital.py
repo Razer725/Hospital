c1dde12e3b8c3fa8e902beb2cdfaac4dc8fe76c0
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-04 14:39:44 +0300

    Update test_hospital.py

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -1,49 +1,45 @@
 import pytest
 
+from exceptions import StatusUpError
 from hospital import Hospital
 
 
 class TestHospital:
-    @pytest.mark.parametrize("patient_id,status,patients", [
-        (1, "Тяжело болен", [0]),
-        (1, "Болен", [1]),
-        (1, "Слегка болен", [2]),
-        (1, "Готов к выписке", [3]),
-    ])
-    def test_get_status(self, patient_id, status, patients):
-        hospital = Hospital(patients=patients)
-        assert hospital.get_status(patient_id) == status
+    def test_get_status(self):
+        hospital = Hospital(patients=[0, 1, 2, 3])
+        assert hospital.get_status(1) == "Тяжело болен"
 
-    @pytest.mark.parametrize("patient_id,status,patients", [
-        (1, "Болен", [0]),
-        # (1, "Готов к выписке", [3])
-    ])
-    def test_status_up(self, patient_id, status, patients):
-        hospital = Hospital(patients=patients)
-        hospital.status_up(patient_id)
-        assert hospital.get_status(patient_id) == status
+    def test_status_up(self):
+        hospital = Hospital(patients=[0, 1, 2, 3])
+        hospital.status_up(1)
+        assert hospital.get_status(1) == 'Болен'
 
-    @pytest.mark.parametrize("patients,count", [([], 0), ([0], 1), ([2, 3], 2)])
-    def test_get_patients_count(self, patients, count):
-        hospital = Hospital(patients=patients)
-        assert hospital.get_patients_count() == count
+    def test_status_up_when_status_max(self):
+        hospital = Hospital(patients=[0, 1, 2, 3])
+        with pytest.raises(StatusUpError):
+            hospital.status_up(4)
 
-    @pytest.mark.parametrize("patients,patients_statuses", [
-        ([0, 1, 2, 3, 0], [('Тяжело болен', 2), ('Болен', 1), ('Слегка болен', 1), ('Готов к выписке', 1)]),
-        ([], []),
-    ])
-    def test_get_patients_statuses(self, patients, patients_statuses):
-        hospital = Hospital(patients=patients)
+    def test_can_status_up(self):
+        hospital = Hospital(patients=[0, 1, 2, 3])
+        assert hospital.can_status_up(1)
+
+    def test_can_status_up_when_status_max(self):
+        hospital = Hospital(patients=[0, 1, 2, 3])
+        assert not hospital.can_status_up(4)
+
+    def test_get_patients_count(self):
+        hospital = Hospital(patients=[0, 1, None, 3])
+        assert hospital.get_patients_count() == 3
+
+    def test__get_patients(self):
+        hospital = Hospital(patients=[0, None, 3])
+        assert hospital._get_patients() == [0, 3]
+
+    def test_get_patients_statuses(self):
+        hospital = Hospital(patients=[0, 1, 2, None, 3, 0])
+        patients_statuses = [('Тяжело болен', 2), ('Болен', 1), ('Слегка болен', 1), ('Готов к выписке', 1)]
         assert hospital.get_patients_statuses() == patients_statuses
 
-
-    # @pytest.mark.parametrize("patient_id,status,patients", [
-    #     (0, "Тяжело болен", [0]),
-    #     (2, "Болен", [1]),
-    #     (-1, "Слегка болен", [2]),
-    #     (1, "Готов к выписке", [3])
-    # ])
-    # def test_get_status_invalid_patient_id(self, patient_id, status, patients):
-    #     hospital = Hospital(patients=patients)
-    #     assert hospital.get_status(patient_id) == status
-
+    def test_get_patients_statuses_without_patients(self):
+        hospital = Hospital(patients=[])
+        assert hospital.get_patients_statuses() == []
