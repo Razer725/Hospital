e6fb92457c7a6e44a078c5000bcb428652fa8c97
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-10 18:32:46 +0300

    Add status up tests

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -1,12 +1,8 @@
 from unittest.mock import MagicMock
 
-import pytest
-
-from exceptions import PatientIdTypeError, PatientMissingError
+from exceptions import PatientIdTypeError
 from hospital import Hospital
 from hospital_commands import HospitalCommands
-from tests.conftest import user_interaction
-from user_interaction import UserInteraction
 
 
 class TestHospitalCommands:
@@ -22,7 +18,8 @@ class TestHospitalCommands:
         user_interaction.request_patient_id = MagicMock(side_effect=PatientIdTypeError)
         hospital_commands = HospitalCommands(Hospital([0, 1, 2]), user_interaction)
         hospital_commands.get_status()
-        user_interaction.send_message.assert_called_once_with("Ошибка. ID пациента должно быть числом (целым, положительным)")
+        user_interaction.send_message.assert_called_once_with(
+            "Ошибка. ID пациента должно быть числом (целым, положительным)")
 
     def test_get_status_when_patient_missing(self):
         user_interaction = MagicMock()
@@ -31,3 +28,25 @@ class TestHospitalCommands:
         hospital_commands.get_status()
         user_interaction.send_message.assert_called_once_with("Ошибка. В больнице нет пациента с таким ID")
 
+    def test_status_up(self):
+        user_interaction = MagicMock()
+        user_interaction.request_patient_id = MagicMock(return_value=1)
+        hospital_commands = HospitalCommands(Hospital([1, 1, 2]), user_interaction)
+        hospital_commands.status_up()
+        user_interaction.send_new_status.assert_called_once_with("Слегка болен")
+
+    def test_status_up_with_max_status_when_discharge_confirmed(self):
+        user_interaction = MagicMock()
+        user_interaction.request_patient_id = MagicMock(return_value=1)
+        hospital_commands = HospitalCommands(Hospital([3, 1, 2]), user_interaction)
+        hospital_commands.status_up()
+        user_interaction.request_discharge_confirmation = MagicMock(return_value=True)
+        user_interaction.send_discharged.assert_called_once()
+
+    def test_status_up_with_max_status_when_discharge_declined(self):
+        user_interaction = MagicMock()
+        user_interaction.request_patient_id = MagicMock(return_value=1)
+        user_interaction.request_discharge_confirmation = MagicMock(return_value=False)
+        hospital_commands = HospitalCommands(Hospital([3, 1, 2]), user_interaction)
+        hospital_commands.status_up()
+        user_interaction.send_status_not_changed.assert_called_once()
