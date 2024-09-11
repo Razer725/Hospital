a210a0a92be29bafbfba20340a58b8260419a113
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-11 12:32:09 +0300

    Add tests for status up with exceptions

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -50,3 +50,18 @@ class TestHospitalCommands:
         hospital_commands = HospitalCommands(Hospital([3, 1, 2]), user_interaction)
         hospital_commands.status_up()
         user_interaction.send_status_not_changed.assert_called_once()
+
+    def test_status_up_when_id_type_invalid(self):
+        user_interaction = MagicMock()
+        user_interaction.request_patient_id = MagicMock(side_effect=PatientIdTypeError)
+        hospital_commands = HospitalCommands(Hospital([0, 1, 2]), user_interaction)
+        hospital_commands.status_up()
+        user_interaction.send_message.assert_called_once_with(
+            "Ошибка. ID пациента должно быть числом (целым, положительным)")
+
+    def test_status_up_when_patient_missing(self):
+        user_interaction = MagicMock()
+        user_interaction.request_patient_id = MagicMock(return_value=10)
+        hospital_commands = HospitalCommands(Hospital([0, 1, 2]), user_interaction)
+        hospital_commands.status_up()
+        user_interaction.send_message.assert_called_once_with("Ошибка. В больнице нет пациента с таким ID")
